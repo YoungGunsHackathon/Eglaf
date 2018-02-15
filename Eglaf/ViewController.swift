cfa5c2c447ca95963f3dd076e2b1ea8d4bf52f75
Adam Zvada <zvadaada@gmail.com>
2018-02-16 00:29:41 +0100

    Initial Commit

---
+++ Eglaf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eglaf
+//
+//  Created by Adam Zvada on 16.02.18.
+//  Copyright © 2018 Adam Zvada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
