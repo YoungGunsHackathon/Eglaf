18d69cd649bd53754b608b88f11f66159338a316
Zvada <adam.zvada@zvada-adam-mac.local>
2018-02-16 16:32:29 +0100

    fonts plus tableviewcell

---
+++ Eglaf/Xib/IssueTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  IssueTableViewCell.swift
+//  Eglaf
+//
+//  Created by Zvada, Adam on 16.02.18.
+//  Copyright © 2018 Adam Zvada. All rights reserved.
+//
+
+import UIKit
+
+class IssueTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
