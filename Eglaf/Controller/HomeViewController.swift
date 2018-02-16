3e90871438488fdeea9a182237ba19e639147021
Zvada <adam.zvada@zvada-adam-mac.local>
2018-02-16 17:54:53 +0100

    finish tabbar implementation

--- Eglaf/Controller/HomeViewController.swift
+++ Eglaf/Controller/HomeViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class HomeViewController: UIViewController, StoryboardInit {
     
+    //MARK: Outlets
+    
     @IBOutlet weak var tableView: UITableView!
     
     //MARK: Life Cycle
@@ -18,6 +20,14 @@ class HomeViewController: UIViewController, StoryboardInit {
     override func viewDidLoad() {
         prepareUI()
         registerXibs()
+        for family: String in UIFont.familyNames
+        {
+            print("\(family)")
+            for names: String in UIFont.fontNames(forFamilyName: family)
+            {
+                print("== \(names)")
+            }
+        }
     }
 }
 
@@ -27,6 +37,23 @@ extension HomeViewController {
     func prepareUI() {
         tableView.delegate = self
         tableView.dataSource = self
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+        self.navigationItem.title = "DASHBOARD"
+        self.navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedStringKey.font: UIFont(name: "SFProDisplay-Regular", size: 14)!,
+            NSAttributedStringKey.foregroundColor: UIColor.white,
+            NSAttributedStringKey.kern: 4
+        ]
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "add"), style: .plain, target: self, action: nil)
+        self.navigationController?.navigationBar.tintColor = UIColor(red:0.35, green:0.43, blue:0.52, alpha:1)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "ALL", style: .plain, target: self, action: nil)
+        self.navigationItem.leftBarButtonItem?.setTitleTextAttributes([
+            NSAttributedStringKey.font: UIFont(name: "SFProDisplay-Regular", size: 14)!,
+            NSAttributedStringKey.foregroundColor: UIColor(red:0.35, green:0.43, blue:0.52, alpha:1),
+            NSAttributedStringKey.kern: 2
+            ], for: .normal)
     }
     func registerXibs() {
         tableView.register(UINib(nibName: "IssueTableViewCell", bundle: nil), forCellReuseIdentifier: "IssueTableViewCell")
