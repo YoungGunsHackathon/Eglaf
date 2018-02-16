246261ebadaac352984f5300d3e9ce97a1a88be9
Zvada <adam.zvada@zvada-adam-mac.local>
2018-02-16 15:52:51 +0100

    add fonts and stuff, idk

--- Eglaf/Controller/HomeViewController.swift
+++ Eglaf/Controller/HomeViewController.swift
@@ -11,4 +11,17 @@ import UIKit
 
 class HomeViewController: UIViewController, StoryboardInit {
     
+    //MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        prepareUI()
+    }
+}
+
+//MARK: - HomeViewController (UI)
+
+extension HomeViewController {
+    func prepareUI() {
+        
+    }
 }
