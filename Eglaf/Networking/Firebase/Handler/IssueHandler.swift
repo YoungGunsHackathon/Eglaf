714ffb6455d1a058981025c6712f44a66216be38
Adam Zvada <zvadaada@gmail.com>
2018-02-16 17:47:19 +0100

    get and observe issues, imageview extension

--- Eglaf/Networking/Firebase/Handler/IssueHandler.swift
+++ Eglaf/Networking/Firebase/Handler/IssueHandler.swift
@@ -12,6 +12,54 @@ class IssueHandler {
     
     static let sharedInstance = IssueHandler()
     
+    var dbProvider: DBProvider = DBProvider.sharedInstance
     
+    
+    func addIssue() {
+        
+    }
+    
+    func getIssue(issueId: String, completion: @escaping (Issue)->Void) {
+        dbProvider.issueRef(issueId: issueId).observeSingleEvent(of: .value) { (snapshot) in
+            if let dictionary = snapshot.value as? [String : Any] {
+                let issue = Issue(id: issueId ,dictionary: dictionary)
+                completion(issue)
+            }
+        }
+    }
+    
+    func observeIssues(completion: @escaping ([Issue]) -> Void) {
+        dbProvider.issueRef.observe(.value) { (snapshot) in
+            var issues: [Issue] = []
+            
+            if let dictionary = snapshot.value as? [String : Any] {
+                for (key, value) in dictionary {
+                    if let data = value as? [String : Any] {
+                        let issue = Issue(id: key, dictionary: data)
+                        issues.append(issue)
+                    }
+                }
+            }
+            completion(issues)
+        }
+    }
+    
+    func observeIssues(category: String, completion: @escaping ([Issue]) -> Void) {
+        dbProvider.issueRef.observe(.value) { (snapshot) in
+            var issues: [Issue] = []
+            
+            if let dictionary = snapshot.value as? [String : Any] {
+                for (key, value) in dictionary {
+                    if let data = value as? [String : Any] {
+                        let issue = Issue(id: key, dictionary: data)
+                        if issue.category == category {
+                            issues.append(issue)
+                        }
+                    }
+                }
+            }
+            completion(issues)
+        }
+    }
     
 }
