7a9bbd886186df8623f2a14f83c48c60c5f806db
Adam Zvada <zvadaada@gmail.com>
2018-02-16 12:45:01 +0100

    Firebase init <3

---
+++ Eglaf/Networking/Firebase/DBProvider.swift
@@ -0,0 +1,35 @@
+//
+//  DBProvider.swift
+//  Eglaf
+//
+//  Created by Adam Zvada on 16.02.18.
+//  Copyright © 2018 Adam Zvada. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+import FirebaseStorage
+
+
+class DBProvider {
+    
+    static let sharedInstance = DBProvider()
+
+    var dbRef : DatabaseReference {
+        return Database.database().reference(fromURL: Constants.DB_URL)
+    }
+    
+    var userRef : DatabaseReference {
+        return dbRef.child(Constants.DatabaseEntities.USER)
+    }
+    
+    var issueRef : DatabaseReference {
+        return dbRef.child(Constants.DatabaseEntities.ISSUE)
+    }
+    
+    var storageRef : StorageReference {
+        return Storage.storage().reference(forURL: Constants.STORAGE_URL)
+    }
+    
+    
+}
---
+++ Eglaf/Networking/Firebase/Handler/UserHandler.swift
@@ -0,0 +1,19 @@
+//
+//  UserHandler.swift
+//  Eglaf
+//
+//  Created by Adam Zvada on 16.02.18.
+//  Copyright © 2018 Adam Zvada. All rights reserved.
+//
+
+import Foundation
+
+class UserHandler {
+    
+    static let sharedInstance = UserHandler()
+    
+    
+    func addUser() {
+        
+    }
+}
