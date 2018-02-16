4554c6464072297d80315571a66137802dd3b99e
Zvada <adam.zvada@zvada-adam-mac.local>
2018-02-16 16:16:18 +0100

    implement tableview with cells

--- Eglaf/Controller/HomeViewController.swift
+++ Eglaf/Controller/HomeViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 
 class HomeViewController: UIViewController, StoryboardInit {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     //MARK: Life Cycle
     
     override func viewDidLoad() {
         prepareUI()
+        registerXibs()
     }
 }
 
@@ -22,6 +25,26 @@ class HomeViewController: UIViewController, StoryboardInit {
 
 extension HomeViewController {
     func prepareUI() {
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    func registerXibs() {
+        tableView.register(UINib(nibName: "IssueTableViewCell", bundle: nil), forCellReuseIdentifier: "IssueTableViewCell")
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 114
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IssueTableViewCell") as! IssueTableViewCell
+        return cell
     }
 }
