46aeec90922f9e89136651134a06a300e92e404d
Zvada <adam.zvada@zvada-adam-mac.local>
2018-02-17 01:13:38 +0100

    implement first half of Profile Screen (needs Jirkas review)

--- Eglaf/Controller/ProfileViewController.swift
+++ Eglaf/Controller/ProfileViewController.swift
@@ -11,4 +11,32 @@ import UIKit
 
 class ProfileViewController: UIViewController, StoryboardInit {
     
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var editButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        prepareNavBar()
+        profileImageView.layer.cornerRadius = profileImageView.frame.size.width / 2.0
+        profileImageView.clipsToBounds = true
+        editButton.backgroundColor = .white
+        editButton.layer.cornerRadius = editButton.frame.size.width / 2.0
+    }
+    
+    func prepareNavBar() {
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0, green: 0.1, blue: 0.22, alpha: 1)
+        //self.navigationController?.view.backgroundColor = UIColor.red
+        self.navigationItem.title = "MY PROFILE"
+        self.navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedStringKey.font: UIFont(name: "SFProDisplay-Regular", size: 14)!,
+            NSAttributedStringKey.foregroundColor: UIColor.white,
+            NSAttributedStringKey.kern: 4
+        ]
+    }
+    
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+        
+    }
 }
