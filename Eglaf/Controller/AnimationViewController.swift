bf2c367ee8e32eb6ab7fb09ccf773ecd5c6073af
Zvada <adam.zvada@zvada-adam-mac.local>
2018-02-17 12:41:51 +0100

    finish animation

--- Eglaf/Controller/AnimationViewController.swift
+++ Eglaf/Controller/AnimationViewController.swift
@@ -12,21 +12,20 @@ import Lottie
 
 class AnimationViewController: UIViewController {
     override func viewDidLoad() {
-        print("CALLED")
     }
     override func viewDidAppear(_ animated: Bool) {
-        print("I was first")
         super.viewDidAppear(animated)
         let animationView = LOTAnimationView(name: "data")
         animationView.loopAnimation = false
+        animationView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
         self.view.addSubview(animationView)
         self.view.bringSubview(toFront: animationView)
         animationView.center = CGPoint(x: view.frame.width / 2.0, y: view.frame.height / 2.0)
         animationView.play { (completion) in
-            UIView.transition(with: self.view, duration: 0.2, options: [.transitionCrossDissolve], animations: {
-                animationView.removeFromSuperview()
-                self.dismiss(animated: true, completion: nil)
-            }, completion: nil)
+            self.view.window?.rootViewController = TabBarViewController()
+//            UIView.transition(with: self.view.window!, duration: 2, options: [.transitionCrossDissolve], animations: {
+//                self.view.window?.rootViewController = TabBarViewController()
+//            }, completion: nil)
         }
     }
 }
